ba2f67d48498ed1c3b6642f4da078e804a1d4d77
pauldevnull <inokien@gmail.com>
2019-06-18 18:50:45 -0500

    incorporated cryptocompare api to calculate total capital from deposits minus withdraws using timestamps

--- trading-stats/trading-stats.js
+++ trading-stats/trading-stats.js
@@ -1,4 +1,5 @@
 const Binance = require('binance-api-node').default
+const bitcoinaverage = require('bitcoinaverage');
 
 const fs = require("fs");
 const contents = fs.readFileSync('../settings.json');
@@ -8,32 +9,43 @@ const binance = Binance({
   apiKey: settings.exchange.binance.key,
   apiSecret: settings.exchange.binance.secret,
   // getTime: xxx // time generator function, optional, defaults to () => Date.now()
-})
+});
+
+const cc = require('cryptocompare')
+cc.setApiKey(settings.exchange.cryptocompare.key);
+
 
 const getDeposits = async() => {
-    const deposits = {};
     const results = await binance.depositHistory();
-    results.depositList.forEach((deposit) => {
-        const { asset, amount } = deposit;
-        deposits[asset] = (asset in deposits) ? deposits[asset] + amount : amount;
-    });
-    return deposits;
+    let totalUSD = 0;
+    let totalBTC = 0;
+    for (const deposit of results.depositList) {
+        const { asset, amount, insertTime } = deposit;
+        const priceHistorical = await cc.priceHistorical(asset, ['USD', 'BTC'], new Date(insertTime)).catch(error => console.log(error));;
+        totalUSD += (priceHistorical.USD * amount) || 0;
+        totalBTC += (priceHistorical.BTC * amount) || 0;
+    }
+    return { USD: totalUSD, BTC: totalBTC };
 }
 
 const getWithdraws = async() => {
-    const withdraws = {};
     const results = await binance.withdrawHistory();
-    results.withdrawList.forEach((withdraw) => {
-        const { asset, amount } = withdraw;
-        withdraws[asset] = (asset in withdraws) ? withdraws[asset] + amount : amount;
-    });
-    return withdraws;
+    let totalUSD = 0;
+    let totalBTC = 0;
+    for (const withdraw of results.withdrawList) {
+        const { asset, amount, successTime } = withdraw;
+        const priceHistorical = await cc.priceHistorical(asset, ['USD', 'BTC'], new Date(successTime)).catch(error => console.log(error));
+        totalUSD += (priceHistorical.USD * amount) || 0;
+        totalBTC += (priceHistorical.BTC * amount) || 0;
+    }
+    return { USD: totalUSD, BTC: totalBTC };
 }
 
 const getTotalFunds = async() => {
-    const deposits = await getDeposits();
-    const withdraws = await getWithdraws();
+    const depositTotal = await getDeposits();
+    const withdrawTotal = await getWithdraws();
 
+    return { USD: (depositTotal.USD - withdrawTotal.USD), BTC: (depositTotal.BTC - withdrawTotal.BTC) };
 }
 
-getTotalFunds().then(result => console.log(''));
+getTotalFunds().then(result => console.log(result));
