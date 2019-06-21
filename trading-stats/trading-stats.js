85ef88a6b9ef1fae7e67a4fe343f9a70003feb8b
pauldevnull <inokien@gmail.com>
2019-06-20 19:34:07 -0500

    added logic to retrieve total invested amount and current total account balance and calculate current profits in both USD and BTC

--- trading-stats/trading-stats.js
+++ trading-stats/trading-stats.js
@@ -1,46 +1,84 @@
+const get = require('lodash/get');
+const pick = require('lodash/pick');
+const omit = require('lodash/omit');
 const Binance = require('binance-api-node').default;
 const fs = require("fs");
 const contents = fs.readFileSync('../settings.json');
 const settings = JSON.parse(contents);
 const binance = Binance({
-	apiKey: settings.exchange.binance.key,
-	apiSecret: settings.exchange.binance.secret,
+    apiKey: settings.exchange.binance.key,
+    apiSecret: settings.exchange.binance.secret,
 });
-
 const cryptocompare = require('cryptocompare');
 cryptocompare.setApiKey(settings.exchange.cryptocompare.key);
+const axios = require('axios');
+const coinMarketCapClient = axios.create({
+    baseURL: 'https://pro-api.coinmarketcap.com/v1',
+    timeout: 1000,
+    headers: {
+        'X-CMC_PRO_API_KEY': settings.exchange.coinmarketcap.key
+    },
+    qs: {
+        start: 1,
+        limit: 5000,
+        convert: 'BTC'
+    },
+});
 
-const getDeposits = async() => {
-    const results = await binance.depositHistory({ useServerTime: true });
-    let totalUSD = 0;
-    let totalBTC = 0;
-    for (const deposit of results.depositList) {
-        const { asset, amount, insertTime } = deposit;
-        const priceHistorical = await cryptocompare.priceHistorical(asset, ['USD', 'BTC'], new Date(insertTime)).catch(error => console.log(error));;
-        totalUSD += (priceHistorical.USD * amount) || 0;
-        totalBTC += (priceHistorical.BTC * amount) || 0;
-    }
-    return { USD: totalUSD, BTC: totalBTC };
+const getTotalInvested = async() => {
+    const options = { useServerTime: true };
+    const currencies = ['USD', 'BTC'];
+    const binancePromises = [binance.depositHistory(options), binance.withdrawHistory(options)];
+    const [ deposits, withdraws ] = await Promise.all(binancePromises);
+    const depositsAndWithdraws = deposits.depositList.concat(withdraws.withdrawList);
+    const historicalPricePromises = depositsAndWithdraws.map((transaction) => {
+        const { asset, amount } = transaction;
+        const transactionDate = new Date(get(transaction, 'insertTime', get(transaction, 'successTime')));
+        return cryptocompare.priceHistorical(asset, currencies, transactionDate).catch(error => console.log(error)).then((result) => {
+            result.isDeposit = 'insertTime' in transaction;
+            currencies.forEach(currency => result[currency] = (result[currency] * amount) || 0);
+            return result;
+        });
+    });
+    const historicalPrices = await Promise.all(historicalPricePromises)
+    return historicalPrices.reduce((acc, cur) => {
+        currencies.forEach(currency => cur.isDeposit ? acc[currency] += cur[currency] : acc[currency] -= cur[currency]);
+        return omit(acc, 'isDeposit');
+    });
 }
 
-const getWithdraws = async() => {
-    const results = await binance.withdrawHistory({ useServerTime: true });
-    let totalUSD = 0;
-    let totalBTC = 0;
-    for (const withdraw of results.withdrawList) {
-        const { asset, amount, successTime } = withdraw;
-        const priceHistorical = await cryptocompare.priceHistorical(asset, ['USD', 'BTC'], new Date(successTime)).catch(error => console.log(error));
-        totalUSD += (priceHistorical.USD * amount) || 0;
-        totalBTC += (priceHistorical.BTC * amount) || 0;
-    }
-    return { USD: totalUSD, BTC: totalBTC };
+const getTotalBalance = async() => {
+    const quotesPromise = coinMarketCapClient.get('https://api.coinmarketcap.com/v2/ticker?convert=BTC').then((result) => {
+        return result.data.data;
+    });
+    const binancePromise = binance.accountInfo({ useServerTime: true }).then((result) => {
+        return result.balances
+    });
+    const [quotes, balances] = await Promise.all([quotesPromise, binancePromise]);
+    const positiveBalances = balances.filter(balance => (parseFloat(balance.free) + parseFloat(balance.locked)) > 0);
+    const currencies = ['USD', 'BTC'];
+    return positiveBalances.reduce((acc, cur) => {
+        const { asset, free, locked } = cur;
+        const total = parseFloat(free) + parseFloat(locked);
+        const quote = Object.values(quotes).filter(quote => !!quote && quote.symbol === asset);
+        currencies.forEach((currency) => {
+            const quotePrice = quote.length && quote[0] && quote[0].quotes[currency].price;
+            if (!acc[currency]) acc[currency] = 0;
+            acc[currency] += (total * quotePrice);
+        });
+        return pick(acc, currencies);
+    });
 }
 
-const getTotalFunds = async() => {
-    const depositTotal = await getDeposits();
-    const withdrawTotal = await getWithdraws();
-
-    return { USD: (depositTotal.USD - withdrawTotal.USD), BTC: (depositTotal.BTC - withdrawTotal.BTC) };
+const getTradingSummary = async() => {
+    const [ totalInvested, totalBalance ] = await Promise.all([getTotalInvested(), getTotalBalance()]);
+    return ['USD', 'BTC'].map((currency) => {
+        const invested = totalInvested[currency];
+        const balance = totalBalance[currency];
+        const profit = balance - invested;
+        return { currency, invested, balance, profit };
+    });
 }
 
-getTotalFunds().then(result => console.log(result));
+
+getTradingSummary().then(result => console.log(result));
